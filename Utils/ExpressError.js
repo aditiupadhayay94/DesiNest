dcf83afe056561bc670c1420edd9a4990e605894
Aditi Upadhayay <upadhyayaditi94@gmail.com>
2025-04-26 00:49:00 +0530

    final2 commit

--- Utils/ExpressError.js
+++ Utils/ExpressError.js
@@ -1,7 +1,11 @@
 class ExpressError extends Error {
-    constructor(message, statusCode) {
-      super(message);
-      this.statusCode = statusCode;
-    }
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = "error";
+    this.isOperational = true; // This helps differentiate between operational errors and programming bugs.
+    Error.captureStackTrace(this, this.constructor);
   }
-  module.exports=ExpressError;
+}
+
+module.exports = ExpressError;
